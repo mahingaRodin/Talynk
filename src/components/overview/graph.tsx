35c1a7eb17d684d395af956913802347d72352b9
NyLaurent <laulanyumbayire@gmail.com>
2025-01-06 20:28:39 -0600

    finished graph section

--- src/components/overview/graph.tsx
+++ src/components/overview/graph.tsx
@@ -1,25 +1,29 @@
-
-// import Sidebar from "./sidebar";
+import { data } from '../../data';
 
 
 const Graph = () => {
   return (
-    <div className="flex w-full">
-  <div className="w-[65%] p-4">
-    
-
-
-
-
-
-       
-        
+    <div className="flex w-full px-4">
+      <div className="w-[70%] p-4">
+        {/* Wrapper for the data items - Flex row layout */}
+        <div className="flex flex-wrap gap-4">
+          {data.map(item => (
+            <div
+              key={item.id}
+              className="flex flex-col items-center rounded-2xl border border-blue p-4 bg-white w-[200px]"
+            >
+              {/* Display the red number and text */}
+              <div className="flex flex-row gap-5">
+                <div className="text-4xl font-bold text-red text-red-600 mb-2">{item.count}</div>
+                <div className="text-lg  font-medium mb-2">{item.text}</div>
+              </div>
+              
+              {/* Display the image dynamically based on item */}
+              <img src={item.image} alt="Graph" className="h-[80px] w-[290px]" />
+            </div>
+          ))}
+        </div>
       </div>
-
-      {/* Sidebar Component - 30% Width */}
-      {/* <div className="w-[30%] bg-gray-200 p-4">
-        <Sidebar />
-      </div> */}
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,39 @@
+// import g1 from '../../assets/g1.svg'; // Import images
+import g2 from './assets/g2.svg';
+import g1 from './assets/g1.svg';
+import g3 from './assets/g3.svg'
+
+export const data = [
+  {
+    id: 1,
+    count: 789,           
+    text: 'Posts',
+    image:g3
+  },
+  {
+    id: 2,
+    count: 32,           
+    text: 'Pending',  
+    image:g3
+  },
+  {
+    id: 3,
+    count: 40,           
+    text: 'Freezed',   
+    image:g2
+  },
+  {
+    id: 4,
+    count: 32,           
+    text: 'Approvers', 
+    image:g1      
+  },
+  {
+    id: 5,
+    count: '5.4K',           
+    text: 'Users',   
+    image:g2     
+  },
+  
+  
+];
