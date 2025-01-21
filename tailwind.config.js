21f56acf9ad105ea1bab0bafcdf6d05738ff0d48
NyLaurent <laulanyumbayire@gmail.com>
2025-01-20 19:57:44 -0600

    shadcn initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    darkMode: ["class"],
+    content: ["./index.html",
+       "./src/**/*.{ts,tsx,js,jsx}"
+      ],
+
   theme: {
-    extend: {
-      colors: {
-        blue: '#006FFD',
-        grey: '#71727A',
-        darkGrey:'#44454A',
-        red:'#FF3B30',
-        black1:'#2F3037',
-        kyan:'#D4E8F6',
-        lightGrey:'#F8F9FE',
-        bgGrey:'#8F9098'
-      },
-    },
+  	extend: {
+  		colors: {
+  			blue: '#006FFD',
+  			grey: '#71727A',
+  			darkGrey: '#44454A',
+  			red: '#FF3B30',
+  			black1: '#2F3037',
+  			kyan: '#D4E8F6',
+  			lightGrey: '#F8F9FE',
+  			bgGrey: '#8F9098',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
     require('tailwind-scrollbar'),
-    
-  ],
+      require("tailwindcss-animate")
+],
 }
 
