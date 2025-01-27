9bff36a380637008879a92a219ff8bbcc7ab4430
NyLaurent <laulanyumbayire@gmail.com>
2025-01-27 16:44:09 -0600

    finished admin portal

--- src/Approvers Portal/Home.tsx
+++ src/Approvers Portal/Home.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import Navbar from "@/components/overview/navbar"
+import Sidebar from "@/components/overview/sidebar"
+import Cards from "../components/Cards"
+import { data as originalData } from "../data"
+import Pending from "@/Reusable/pending"
+import Videos from '../components/overview/Videos'
+
 
 const Home = () => {
+    const limitedData = originalData.slice(0, 5);
   return (
-    <div>Home</div>
+    <div>
+        <div>
+            <Navbar></Navbar>
+        </div>
+        <div className="flex justify-between px-9 ">
+        <div className="">
+            <div>
+                <h1 className="text-2xl font">Hello, <span className="text-blue font-bold">Nyumbayire</span></h1><br />
+                <h2 className="text-lg">Glad to have you on board😀</h2>
+            </div>
+            <div>
+                <Cards data={limitedData} extraStyles="gap-7 pt-3"></Cards>
+            </div>
+            <div>
+                <Pending></Pending>
+            </div>
+            <div>
+                <Videos></Videos>
+                <Videos></Videos>
+            </div>
+           
+           
+
+            
+        </div>
+        
+        <div>
+            <Sidebar></Sidebar>
+        </div>
+
+        </div>
+       
+
+
+
+
+    </div>
   )
 }
 
